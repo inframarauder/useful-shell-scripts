a2f7b0ab7edc3dc55f2fd8649b7e02e89dca1746
Subhasis Das <subhasisdas125@gmail.com>
2021-01-20 23:36:39 +0530

    added codedeploy.sh

---
+++ install-scripts/codedeploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+##shell script to install and configure codedeploy agent on AWS EC2 instance (Ubuntu) :
+
+sudo apt-get update
+sudo apt-get install ruby
+sudo apt-get install wget
+cd /home/ubuntu
+wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto > /tmp/logfile
+sudo service codedeploy-agent start
