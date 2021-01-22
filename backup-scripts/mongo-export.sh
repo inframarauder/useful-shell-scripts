fe7158b56a966e370e1be9f3edeac0e11eed6af0
subhasis das <subhasisdas125@gmail.com>
2021-01-22 23:08:48 +0530

    mongo-export script done

--- backup-scripts/mongo-export.sh
+++ backup-scripts/mongo-export.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-##shell script to export dump json from mongo collection
+##shell script to export all collections of a db as json 
 
-echo "Enter DB Name"
-read dbName
-echo "Enter collection Name"
-read collectionName
-echo "Enter output path"
-read outputPath
+dbName=$1
 
-sudo mongoexport --db $dbName --collection $collectionName --out $outputPath
+if [ -z "$dbName" ];
+then
+    echo "Please enter db name"
+else
+    collections=$(mongo $dbName --quiet --eval "db.getCollectionNames()") ##returns a string (not array) [c1,c2,c3..so on]
+
+    for col in $collections;
+    do
+        if [[ "$col" == "[" || "$col" == "]" ]];then
+            continue
+        fi
+        col=${col:1}
+        col=${col::-2}
+        mongoexport --db=$dbName --collection=$col --out="mongoexports/"$col"."json
+    done
+fi
